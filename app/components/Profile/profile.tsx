f3335f30b4a60f05268302c071505570abf68c91
Mauricio <mghb1985@gmail.com>
2024-05-25 10:08:34 -0300

    Se crea boton subir foto

--- app/components/Profile/profile.tsx
+++ app/components/Profile/profile.tsx
@@ -9,10 +9,12 @@ import LocationIcon from '@/assets/svg/place.svg';
 import FingerprintIcon from '@/assets/svg/fingerprint.svg';
 import CalendarIcon from '@/assets/svg/calendar.svg';
 import EmailIcon from '@/assets/svg/email.svg';
-import { FormControl, InputLabel, Input, Link } from '@mui/material';
+import { FormControl, InputLabel, Input, Link, styled, IconButton } from '@mui/material';
 import FormContainer from '../FormContainer/formContainer';
 import { useEffect, useState } from 'react';
-import { editarPerfilFetch, editarUsuarioFetch, obtenerDatosUsuarioFetch } from '@/lib/data/actions';
+import { editarUsuarioFetch, obtenerDatosUsuarioFetch } from '@/lib/data/actions';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import ButtonMaterial from '@mui/material/Button';
 
 function EditarPerfilButton() {
   const { pending } = useFormStatus();
@@ -24,7 +26,7 @@ function EditarPerfilButton() {
 }
 
 export default function Profile() {
-  
+
   const user: User = {
     nombre: 'Jane',
     apellido: 'Doe',
@@ -45,32 +47,31 @@ export default function Profile() {
 
   useEffect(() => {
     obtenerDatosUsuarioFetch()
-        .then(u => {
-          setUsuario(u);
-          setLoading(false);
-          console.log('USUARIO', u);
-        })
-        .catch(error => {
-            setError(error);
-            setLoading(false);
-        });
-  }, []); 
+      .then(u => {
+        setUsuario(u);
+        setLoading(false);
+        console.log('USUARIO', u);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
 
   const handleClickEditar = () => {
-    if(datosUsuario.telefono !== '' && datosUsuario.domicilio !== '' && datosUsuario.imagen !== '')
-    {      
+    if (datosUsuario.telefono !== '' && datosUsuario.domicilio !== '' && datosUsuario.imagen !== '') {
       editarUsuarioFetch(datosUsuario.telefono, datosUsuario.domicilio, datosUsuario.imagen)
-        .then(()=>{
-          setEditado(true);          
-      });
+        .then(() => {
+          setEditado(true);
+        });
     }
   }
 
 
 
   const handleChange = (name: string, newValue: string) => {
-    if(name === 'telefono') setUsuario({ ...datosUsuario, telefono: newValue });
-    if(name === 'domicilio') setUsuario({ ...datosUsuario, domicilio: newValue });
+    if (name === 'telefono') setUsuario({ ...datosUsuario, telefono: newValue });
+    if (name === 'domicilio') setUsuario({ ...datosUsuario, domicilio: newValue });
     //if(name === 'imagen') setUsuario({ ...datosUsuario, imagen: newValue });
   };
 
@@ -80,31 +81,85 @@ export default function Profile() {
 
   if (error) {
     return <p>Error al cargar los datos: {error}</p>;
-  }  
+  }
 
-  const urlImagen = 'https://i.ebayimg.com/images/g/RNsAAOSwGotWq6ju/s-l500.jpg';
-  
-  return (    
+  const handlefile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      // Aquí puedes manejar el archivo
+      console.log('Archivo seleccionado:', file);
+      // Puedes leer el contenido del archivo si es necesario
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const content = e.target?.result;
+        console.log('Contenido del archivo:', file.name);
+      };
+      reader.readAsText(file); // Puedes cambiar readAsText a otro método dependiendo del tipo de archivo
+    }
+  };
+
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+
+  return (
     <FormContainer>
       <div>
-        <div style={{ display: 'inline-block', alignContent: 'center'}}>
-          <Image
-            loader={() => urlImagen}
-            src={urlImagen}
-            alt='user image'
-            width={200}
-            height={100}
-            style={{
-              objectFit: 'cover',
-              height: '200px',
-              borderRadius: '50%',
-              padding: '10px',
-            }}
-          />
+        <div>
+          <div style={{ display: 'inline-block', verticalAlign: 'center' }}>
+            <Image
+              loader={() => datosUsuario?.imagen}
+              src={datosUsuario?.imagen}
+              alt='user image'
+              width={200}
+              height={100}
+              style={{
+                objectFit: 'cover',
+                height: '200px',
+                borderRadius: '50%',
+                padding: '10px',
+                display: 'inline-block'
+              }}
+            />
+            <div id="divNombreCabezal" style={{ display: 'inline-block', verticalAlign: 'middle', height: '100%' }}>
+              <h3>{datosUsuario?.nombre} {datosUsuario?.apellido}</h3>
+            </div>
+          </div>
+          <div style={{ marginLeft: '80px', marginTop: '-50px' }}>
+            <>
+              <input
+                style={{ display: "none" }}
+                accept="image/png, image/jpeg" 
+                id="choose-file"
+                type="file"
+                onChange={(event) => handlefile(event)}
+              />
+              <label htmlFor="choose-file">
+                <IconButton aria-label="upload" component="span">
+                  <CameraAltIcon />
+                </IconButton>
+              </label>
+            </>
+            {/* <ButtonMaterial
+              component="label"
+              role={undefined}
+              variant="text"
+              tabIndex={-1}
+              startIcon={<CameraAltIcon fontSize='medium'/>}
+            >
+              <VisuallyHiddenInput type="file" />
+            </ButtonMaterial> */}
+          </div>
         </div>
-        <div id="divNombreCabezal" style={{ display: 'inline-block', verticalAlign: 'middle', height: '100%' }}>
-          <h3>{datosUsuario?.nombre} {datosUsuario?.apellido}</h3>
-        </div>
+
         <div>
           <h6 style={{
             objectFit: 'cover',
@@ -124,7 +179,7 @@ export default function Profile() {
             </div>
             <FormControl variant="standard" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
               <InputLabel htmlFor="component-simple">Nombre</InputLabel>
-              <Input id="component-simple" name="nombre" value={datosUsuario?.nombre} size='small'  />
+              <Input id="component-simple" name="nombre" value={datosUsuario?.nombre} size='small' />
             </FormControl>
           </div>
 
@@ -154,16 +209,16 @@ export default function Profile() {
             </div>
             <FormControl variant="standard" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
               <InputLabel htmlFor="component-simple">Telefono</InputLabel>
-              <Input 
-                id="component-simple" 
-                name="telefono" 
-                value={datosUsuario?.telefono} 
-                size='small' 
-                onChange={(e) => handleChange(e.target.name, e.target.value)} 
-                readOnly={false} 
+              <Input
+                id="component-simple"
+                name="telefono"
+                value={datosUsuario?.telefono}
+                size='small'
+                onChange={(e) => handleChange(e.target.name, e.target.value)}
+                readOnly={false}
                 inputProps={{
                   inputMode: 'numeric',
-                }}/>
+                }} />
             </FormControl>
           </div>
 
@@ -202,7 +257,7 @@ export default function Profile() {
             </div>
             <FormControl variant="standard" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
               <InputLabel htmlFor="component-simple">Domicilio</InputLabel>
-              <Input id="component-simple" name="domicilio" value={datosUsuario?.domicilio} size='small' onChange={(e) => handleChange(e.target.name, e.target.value)}/>
+              <Input id="component-simple" name="domicilio" value={datosUsuario?.domicilio} size='small' onChange={(e) => handleChange(e.target.name, e.target.value)} />
             </FormControl>
           </div>
         </div>
