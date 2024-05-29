afc2f976a5b4d879ce56587a1b3aa5ff265867bc
Adroda <adrianchi_91@hotmail.com>
2024-05-29 20:09:26 -0300

    minor fixes

--- app/funcionario/horarios/periodos-examenes/agregar/page.tsx
+++ app/funcionario/horarios/periodos-examenes/agregar/page.tsx
@@ -25,56 +25,56 @@ function Form({ carrera }: any) {
   return (
     <FormContainer>
       <form
-        className="flex min-h-full w-full flex-col items-center justify-between gap-1 md:mx-auto md:h-full md:max-w-full md:gap-2 md:px-6"
+        className='flex min-h-full w-full flex-col items-center justify-between gap-1 md:mx-auto md:h-full md:max-w-full md:gap-2 md:px-6'
         action={dispatch}
       >
-        <h1 className="pb-4 text-center text-2xl font-bold leading-snug text-black">
+        <h1 className='pb-4 text-center text-2xl font-bold leading-snug text-black'>
           Agregar periodo de examen de la carrera {carrera.nombre}
         </h1>
         <InputField
-          type="date"
-          name="fechaInicio"
-          label="Fecha de inicio del periodo"
-          pattern="dd-mm-yyyy"
+          type='date'
+          name='fechaInicio'
+          label='Fecha de inicio del periodo'
+          pattern='dd-mm-yyyy'
         ></InputField>
-        <div id="fechaInicio-error" aria-live="polite" aria-atomic="true">
+        <div id='fechaInicio-error' aria-live='polite' aria-atomic='true'>
           {registro?.errors?.fechaInicio &&
             registro.errors.fechaInicio.map((error: string) => (
-              <p className="mt-2 text-sm text-garnet" key={error}>
+              <p className='mt-2 text-sm text-garnet' key={error}>
                 {error}
               </p>
             ))}
         </div>
         <InputField
-          type="date"
-          name="fechaFin"
-          label="Fecha de fin del periodo"
-          pattern="dd-mm-yyyy"
+          type='date'
+          name='fechaFin'
+          label='Fecha de fin del periodo'
+          pattern='dd-mm-yyyy'
         ></InputField>
-        <div id="fechaFin-error" aria-live="polite" aria-atomic="true">
+        <div id='fechaFin-error' aria-live='polite' aria-atomic='true'>
           {registro?.errors?.fechaFin &&
             registro.errors.fechaFin.map((error: string) => (
-              <p className="mt-2 text-sm text-garnet" key={error}>
+              <p className='mt-2 text-sm text-garnet' key={error}>
                 {error}
               </p>
             ))}
         </div>
         <InputField
-          type="number"
-          name="carreraId"
-          label="Carrera ID"
-          className="hidden"
+          type='number'
+          name='carreraId'
+          label='Carrera ID'
+          className='hidden'
           value={carrera.id}
         />
-        <div id="carreraId-error" aria-live="polite" aria-atomic="true">
+        <div id='carreraId-error' aria-live='polite' aria-atomic='true'>
           {registro?.errors?.carreraId &&
             registro.errors.carreraId.map((error: string) => (
-              <p className="mt-2 text-sm text-garnet" key={error}>
+              <p className='mt-2 text-sm text-garnet' key={error}>
                 {error}
               </p>
             ))}
         </div>
-        <div className="flex w-2/3 flex-col justify-between gap-1 sm:w-full sm:flex-row">
+        <div className='flex w-2/3 flex-col justify-between gap-1 sm:w-full sm:flex-row'>
           <RegistrarPeriodoButton />
         </div>
       </form>
@@ -85,7 +85,7 @@ function Form({ carrera }: any) {
 function RegistrarPeriodoButton() {
   const { pending } = useFormStatus();
   return (
-    <Button className="w-full" styling="primary" disabled={pending}>
+    <Button className='w-full' styling='primary' disabled={pending}>
       {pending ? 'Registrando...' : 'Registrar'}
     </Button>
   );
@@ -103,8 +103,8 @@ export default function FuncionarioHorariosExamenesAgregarHome() {
   });
 
   return (
-    <section className="text-ivory">
-      <Form carrera={carrera} className="inset-1/2" />
+    <section className='text-ivory'>
+      <Form carrera={carrera} className='inset-1/2' />
     </section>
   );
 }
--- app/funcionario/horarios/periodos-examenes/page.tsx
+++ app/funcionario/horarios/periodos-examenes/page.tsx
@@ -3,7 +3,7 @@
 export default function FuncionarioHorariosExamenesHome() {
   return (
     <section className="text-ivory">
-      <h1>Funcionario/Horarios/Examenes</h1>
+      <h1>Funcionario/Horarios/Periodo-Examenes</h1>
     </section>
   );
 }
