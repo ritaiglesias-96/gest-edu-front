fe3030868ef7e0e012c6de6db727bccde4616725
adroda <adrian.gioda@gmail.com>
2024-06-04 20:18:10 -0300

    Borrada formatDate que ya se habia movido en otro pr

--- app/funcionario/horarios/[id]/[asignaturaId]/page.tsx
+++ app/funcionario/horarios/[id]/[asignaturaId]/page.tsx
@@ -41,22 +41,6 @@ export default function AsignaturaPage({
     fetch().finally(() => setLoading(false));
   }, [params.asignaturaId]);
 
-  function formatDate(dateString: string): string {
-    const parts = dateString.split('T');
-    if (parts.length !== 2) {
-      throw new Error('Invalid date format. Expected YYYY-MM-DDTHH:mm');
-    }
-
-    const datePart = parts[0];
-    const [year, month, day] = datePart.split('-');
-
-    // Ensure two-digit formatting for month and day
-    const formattedMonth = month.padStart(2, '0');
-    const formattedDay = day.padStart(2, '0');
-
-    return `${formattedDay}/${formattedMonth}/${year}`;
-  }
-
   function formatDateCurso(dateString: string): string {
     // Check for empty string input (optional)
     if (!dateString) {
