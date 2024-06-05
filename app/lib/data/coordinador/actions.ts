464b34c308a9832362578baf88551b22cd7e1440
Rita <59368559+ritaiglesias-96@users.noreply.github.com>
2024-06-04 23:03:41 -0300

    Revert "Revert "Feature/inscripcion carrera funcionario [WIP]" (#26)" (#28)
    This reverts commit 91bcbde3709983e459c8b41a6844c4458e11a7cc.

--- app/lib/data/coordinador/actions.ts
+++ app/lib/data/coordinador/actions.ts
@@ -266,6 +266,7 @@ export async function getPrevituras(id: string) {
     });
     if (previas.ok) {
       const data = await previas.json();
+      console.log(data);
       return data;
     } else {
       return null;
@@ -286,6 +287,7 @@ export async function getNoPrevituras(id: string) {
     });
     if (noPrevias.ok) {
       const data = await noPrevias.json();
+      console.log(data);
       return data;
     } else {
       return null;
@@ -333,7 +335,6 @@ export async function altaPreviaFetch(asignaturaId: string, previaId: string) {
     );
     if (previaturas.ok) {
       console.log(previaturas);
-
       return {
         message: 'Previa creada con exito.',
       };
