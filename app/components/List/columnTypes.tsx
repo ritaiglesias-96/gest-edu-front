ce831723ea7ef3d1e0e8d8fc5d3eab47a0f048f0
adroda <adrian.gioda@gmail.com>
2024-06-10 21:34:18 -0300

    saving work done

--- app/components/List/columnTypes.tsx
+++ app/components/List/columnTypes.tsx
@@ -631,7 +631,6 @@ export const asignaturaBajaCursoColumns: GridColDef[] = [
         styling='outline'
         onClick={async () => {
           const response = await bajaCursoFetch(params.id.toString());
-          console.log(response);
           if (response) {
             alert(response.message);
           }
