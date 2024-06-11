3ff66692d8383082e83bc5b6c49617650c4127c9
Mauricio <30642578+mauri1985@users.noreply.github.com>
2024-06-10 21:24:41 -0300

    feature/ListarAsignaturasPendientesAprobacion (#39)

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -244,6 +244,11 @@ export type CarreraAsignaturas = {
   asignaturas: Asignatura[];
 };
 
+export type CarreraAsignaturas = {
+  carrera: Carrera;
+  asignaturas: Asignatura[];
+}
+
 export interface InscripcionCarreraPendienteFlattened {
   id: number;
   motivoRechazo: string;
