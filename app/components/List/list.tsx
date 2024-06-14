34a49b0ecbf4ddc9d6cb9b0894c850472b684e8c
Adroda <adrianchi_91@hotmail.com>
2024-06-14 00:12:00 -0300

    fix

--- app/components/List/list.tsx
+++ app/components/List/list.tsx
@@ -17,14 +17,6 @@ import {
   asignaturaExamenColumns,
   asignaturaCursoColumns,
   examenColumns,
-  solicitudTituloColumns,
-  carrerasFuncionario,
-  carreraInscripcionFuncionarioColumns,
-  asignaturaExamenFuncionarioColumns,
-  ExamenFuncionarioColumns,
-  InscriptosExamenFuncionarioColumns,
-  asignaturaBajaCursoColumns,
-  solicitudTituloColumns,
   carrerasFuncionario,
   carreraInscripcionFuncionarioColumns,
   asignaturaExamenFuncionarioColumns,
@@ -110,13 +102,13 @@ type columnType =
   | 'carreraFuncionario'
   | 'asignaturaFuncionario'
   | 'calficar-cursos'
-  | 'solicitudTitulo'
   | 'carreras-funcionario'
   | 'carreraInscripcionFuncionario'
   | 'asignaturaExamenFuncionario'
   | 'examenFuncionario'
   | 'inscriptosExamenFuncionario'
   | 'asignaturaBajaCurso'
+  | 'solicitudTitulo'
   | 'none';
 interface ListProps {
   isEditableDocentes?: boolean;
@@ -125,8 +117,8 @@ interface ListProps {
   isEditableAsignaturas?: boolean;
   editarCalificacionCurso?: boolean;
   isApproveRejectCarrera?: boolean;
-  isSolicitudTramite?: boolean;
   isHorarioCursoConsulta?: boolean;
+  isSolicitudTramite?: boolean;
   rows: GridRowsProp[];
   rowsLoading: boolean;
   columnsType: columnType;
@@ -139,8 +131,8 @@ export default function List({
   isEditableAsignaturas,
   editarCalificacionCurso,
   isApproveRejectCarrera,
-  isSolicitudTramite,
   isHorarioCursoConsulta,
+  isSolicitudTramite,
   rows,
   rowsLoading,
   columnsType,
@@ -218,20 +210,6 @@ export default function List({
       break;
     case 'solicitudTitulo':
       columns = solicitudTituloColumns;
-    case 'carreraInscripcionFuncionario':
-      columns = carreraInscripcionFuncionarioColumns;
-      break;
-    case 'asignaturaExamenFuncionario':
-      columns = asignaturaExamenFuncionarioColumns;
-      break;
-    case 'examenFuncionario':
-      columns = ExamenFuncionarioColumns;
-      break;
-    case 'inscriptosExamenFuncionario':
-      columns = InscriptosExamenFuncionarioColumns;
-      break;
-    case 'asignaturaBajaCurso':
-      columns = asignaturaBajaCursoColumns;
       break;
     default:
       break;
@@ -300,12 +278,6 @@ export default function List({
       )}
       {isSolicitudTramite && (
         <SolicitudTramiteDataGrid
-          rowsParent={rows}
-          rowsLoadingParent={rowsLoading}
-        />
-      )}
-      {isHorarioCursoConsulta && (
-        <HorariosCursosEstudiante
           rowsParent={rows}
           rowsLoadingParent={rowsLoading}
         />
@@ -1378,6 +1350,161 @@ function InscripcionCursoDataGrid({
   );
 }
 
+function HorariosCursosEstudiante({
+  rowsParent,
+  rowsLoadingParent,
+}: Readonly<{
+  rowsParent: GridRowsProp;
+  rowsLoadingParent: boolean;
+}>) {
+  const [rows, setRows] = useState<GridRowsProp>([]);
+  const [rowsLoading, setRowsLoading] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [horarios, setHorarios] = useState<HorarioCurso[]>([]);
+
+  useEffect(() => {
+    //Se convierte la fecha a formato dd/MM/yyyy
+    rowsParent.forEach((curso) => {
+      curso.fechaInicio = convertirFecha(curso.fechaInicio);
+      curso.fechaFin = convertirFecha(curso.fechaFin);
+    });
+    setRows(rowsParent);
+    setRowsLoading(rowsLoadingParent);
+  }, [rowsLoadingParent, rowsParent]);
+
+  const handleHorario = (id: string) => {
+    if (rows) {
+      const cursoEncontrados = rows.find((c) => c.id.toString() === id);
+      if (cursoEncontrados) {
+        const horarioEncontrado = cursoEncontrados.horarios;
+        setHorarios(horarioEncontrado);
+      }
+    }
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      cellClassName: 'flex items-center self-end',
+      headerClassName: 'header-center',
+      flex: 1,
+    },
+    {
+      field: 'asignaturaNombre',
+      headerName: 'Asignatura',
+      align: 'center',
+      headerAlign: 'center',
+      flex: 1,
+    },
+    {
+      field: 'docente',
+      headerName: 'Docente',
+      align: 'center',
+      headerAlign: 'center',
+      flex: 1,
+    },
+    {
+      field: 'fechaInicio',
+      headerName: 'Fecha de Inicio',
+      align: 'center',
+      headerAlign: 'center',
+      flex: 1,
+    },
+    {
+      field: 'fechaFin',
+      headerName: 'Fecha de Fin',
+      align: 'center',
+      headerAlign: 'center',
+      flex: 1,
+    },
+    {
+      field: 'horarios',
+      headerName: 'Horarios',
+      align: 'center',
+      headerAlign: 'center',
+      flex: 1,
+      renderCell: (params) => (
+        <Link
+          href={`${window.location.pathname}`}
+          onClick={() => {
+            setIsOpen(true), handleHorario(params.id.toString());
+          }}
+          className='mx-auto flex size-fit'
+        >
+          <Schedule className='h-auto w-6 fill-garnet sm:w-8' />
+        </Link>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <div>
+        <DataGrid
+          className='w-full'
+          rows={rows}
+          loading={rowsLoading}
+          columns={columns}
+          sx={{ backgroundColor: '#f6f6e9', color: 'black' }}
+        />
+      </div>
+      {isOpen && (
+        <div className='absolute left-1/2 top-1/2 max-w-2xl -translate-x-1/2 -translate-y-1/2 rounded-md bg-ivory px-4 py-2 shadow-lg shadow-garnet'>
+          <div className='my-2 box-content items-center justify-between rounded-md bg-ivory px-4 py-2 md:flex-row md:align-baseline'>
+            <div className='rounded-md text-center font-bold text-black'>
+              <h5 className='m-0 p-0'>Horarios</h5>
+              {horarios.length > 0 ? (
+                <div>
+                  {horarios.map((h, index) => (
+                    <TableContainer
+                      key={`${h.dia}-${h.horaInicio}-${h.horaFin}-${index}`}
+                    >
+                      <Table aria-label='simple table'>
+                        <TableBody>
+                          <TableRow
+                            key={h.dia}
+                            sx={{
+                              '&:last-child td, &:last-child th': { border: 0 },
+                              py: 1,
+                            }}
+                          >
+                            <TableCell align='right'>
+                              <span className='font-bold text-black'>
+                                {h.dia}
+                              </span>
+                              : {h.horaInicio} - {h.horaFin}
+                            </TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  ))}
+                </div>
+              ) : (
+                <p className='text-black'>
+                  No hay horarios ingresados para el curso
+                </p>
+              )}
+              <div className='items-center md:space-x-6'>
+                <div className='inline-block'>
+                  <Button
+                    onClick={() => setIsOpen(false)}
+                    className='lg:w-200'
+                    styling='primary'
+                  >
+                    Cerrar
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
 function SolicitudTramiteDataGrid({
   rowsParent,
   rowsLoadingParent,
@@ -1534,140 +1661,6 @@ function SolicitudTramiteDataGrid({
             {mensajeError}
           </Alert>
         </Collapse>
-      )}
-    </>
-  );
-}
-
-function HorariosCursosEstudiante({
-  rowsParent,
-  rowsLoadingParent,
-}: Readonly<{
-  rowsParent: GridRowsProp;
-  rowsLoadingParent: boolean;
-}>) {
-  const [rows, setRows] = useState<GridRowsProp>([]);
-  const [rowsLoading, setRowsLoading] = useState(true);
-  const [isOpen, setIsOpen] = useState(false);
-  const [horarios, setHorarios] = useState([]);
-
-  useEffect(() => {
-    //Se convierte la fecha a formato dd/MM/yyyy
-    rowsParent.forEach((curso) => {
-      curso.fechaInicio = convertirFecha(curso.fechaInicio);
-      curso.fechaFin = convertirFecha(curso.fechaFin);
-    });
-    setRows(rowsParent);
-    setRowsLoading(rowsLoadingParent);
-  }, [rowsLoadingParent, rowsParent]);
-
-  const handleHorario = (id: string) => {
-    if (rows) {
-      const cursoEncontrados = rows.find((c) => c.id.toString() === id);
-      if (cursoEncontrados) {
-        const horarioEncontrado = cursoEncontrados.horarios;
-        setHorarios(horarioEncontrado);
-      }
-    }
-  };
-
-  const columns: GridColDef[] = [
-    {
-      field: 'id',
-      headerName: 'ID',
-      cellClassName: 'flex items-center self-end',
-      headerClassName: 'header-center',
-      flex: 1,
-    },
-    {
-      field: 'asignaturaNombre',
-      headerName: 'Asignatura',
-      align: 'center',
-      headerAlign: 'center',
-      flex: 1,
-    },
-    {
-      field: 'docente',
-      headerName: 'Docente',
-      align: 'center',
-      headerAlign: 'center',
-      flex: 1,
-    },
-    {
-      field: 'fechaInicio',
-      headerName: 'Fecha de Inicio',
-      align: 'center',
-      headerAlign: 'center',
-      flex: 1,
-    },
-    {
-      field: 'fechaFin',
-      headerName: 'Fecha de Fin',
-      align: 'center',
-      headerAlign: 'center',
-      flex: 1,
-    },
-    {
-      field: 'horarios',
-      headerName: 'Horarios',
-      align: 'center',
-      headerAlign: 'center',
-      flex: 1,
-      renderCell: (params) => (
-        <Link
-          href={`${window.location.pathname}`}
-          onClick={() => {
-            setIsOpen(true), handleHorario(params.id.toString());
-          }}
-          className='mx-auto flex size-fit'
-        >
-          <Schedule className='h-auto w-6 fill-garnet sm:w-8' />
-        </Link>
-      ),
-    },
-  ];
-
-  return (
-    <>
-      <div>
-        <DataGrid
-          className='w-full'
-          rows={rows}
-          loading={rowsLoading}
-          columns={columns}
-          sx={{ backgroundColor: '#f6f6e9', color: 'black' }}
-        />
-      </div>
-      {isOpen && (
-        <div className='absolute left-1/2 top-1/2 max-w-2xl -translate-x-1/2 -translate-y-1/2 rounded-md bg-ivory px-4 py-2 shadow-lg shadow-garnet'>
-          <div className='my-2 box-content items-center justify-between rounded-md bg-ivory px-4 py-2 md:flex-row md:align-baseline'>
-            <div className='rounded-md text-center font-bold text-black'>
-              <h5 className='m-0 p-0'>Horarios</h5>
-              {horarios.length > 0 ? (
-                <p>
-                  {horarios.map((h) => (
-                    <div>
-                      {h.dia} {h.horaInicio} {h.horaFin}
-                    </div>
-                  ))}
-                </p>
-              ) : (
-                <h1>no hay...</h1>
-              )}
-              <div className='items-center md:space-x-6'>
-                <div className='inline-block'>
-                  <Button
-                    onClick={() => setIsOpen(false)}
-                    className='lg:w-20'
-                    styling='primary'
-                  >
-                    Cerrar
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       )}
     </>
   );
