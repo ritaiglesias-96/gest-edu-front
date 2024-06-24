e69ccd12a261735170ace4ce8f0ed7bfe9c2d26e
Mauricio <mghb1985@gmail.com>
2024-06-24 00:19:46 -0300

    Se completa instructivos

--- app/components/Instructivos/instructivo.tsx
+++ app/components/Instructivos/instructivo.tsx
@@ -9,6 +9,8 @@ import MuiAccordionSummary, {
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import Grading from '@/assets/svg/grading.svg';
+import Edit from '@/assets/svg/edit.svg';
+import School from '@/assets/svg/school.svg';
 
 const Accordion = styled((props: AccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -47,7 +49,7 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 }));
 
 export default function InstructivosPage() {
-  const [expanded, setExpanded] = React.useState<string | false>('panel1');
+  const [expanded, setExpanded] = React.useState<string | false>('');
 
   const handleChange =
     (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
@@ -97,14 +99,37 @@ export default function InstructivosPage() {
         onChange={handleChange('panel2')}
       >
         <AccordionSummary aria-controls='panel2d-content' id='panel2d-header'>
-          <Typography>Collapsible Group Item #2</Typography>
+          <Typography>Solicitudes</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
+            <div className='flex'>
+              <School className='h-6 self-center sm:w-auto mr-5' />
+              <h6>Solicitud de título</h6>
+            </div>
+            <p>
+              En la seccion del solicitudes, el estudiante puede ver las
+              carreras que se encuentra inscripto y aun no han finalizado.
+              Haciendo clic en el botón solicitar título, se envía una
+              solicitud, la cual debe ser aprobada por el coordinador de la
+              carrera. En caso de ser rechazada, se muestra un mensaje indicando
+              el motivo, por ejemplo, no tiene los créditos suficientes o no
+              tiene aprobadas todas las asignaturas. Tanto cuando es aprobada
+              como rechazada, el estudiante recibe una triple notificación, por
+              correo electrónico, notificaciónweb y mobile.
+            </p>
+            <hr />
+            <div className='flex'>
+              <Grading className='h-6 self-center sm:w-auto mr-5' />
+              <h6>Solicitud de certificado</h6>
+            </div>
+            <p>
+              En cuanto a la solicitud de certificado, se puede descargar en
+              formato PDF, el cual contiene un código para que pueda ser
+              validado por la persona u organización que recibe el certificado.
+              Ingresando dicho código en la página como invitado, puede ver los
+              datos del certificado y comprobar que corresponde al estudiante.
+            </p>
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -113,14 +138,44 @@ export default function InstructivosPage() {
         onChange={handleChange('panel3')}
       >
         <AccordionSummary aria-controls='panel3d-content' id='panel3d-header'>
-          <Typography>Collapsible Group Item #3</Typography>
+          <Typography>Inscripciones</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
+            En la sección inscripciones, el estudiante puede inscribirse tanto a
+            carrras, como cursos y exámenes. Para ello, se divide en dos
+            secciones mas.
+            <div className='flex'>
+              <Edit className='h-6 self-center sm:w-auto mr-5' />
+              <h6>Mis carreras</h6>
+            </div>
+            <p>
+              Muestra las carreras que se encuentra inscripto, y dentro de éste
+              listado, es posible anotarse tanto a cursos como exámenes. Al
+              hacer clic en cursos, es redirigido a otra página, donde puede
+              seleccionar el curso que desea filtrando por asignatura. Tambien
+              es posible darse de baja si ya se encuentra inscripto en un curso
+              que aun no ha comenzado. En el caso de exámenes, tambien es
+              redirigido a otra página, donde se muestra los exámenes
+              disponibles para inscribirse. Luego de acceder al exámen se
+              muestran dos botones, para inscribirse o darse de baja. Al hacer
+              click en alguno de éstos botones, se solicita confirmación tanto
+              para inscribirse como para darse de baja.
+            </p>
+            <div className='flex'>
+              <Edit className='h-6 self-center sm:w-auto mr-5' />
+              <h6>Otras carreras</h6>
+            </div>
+            <p>
+              Son las carreras que el estudiante no se encuentra inscripto,
+              donde se muestra la lista de carreras disponibles y en dicha lista
+              se encuentra un boton, donde al hacer click se muestra mensaje de
+              confirmación para inscribirse a la carrera. Luego de realizada la
+              inscripción, esta debe ser aprobada por un coordiador, y
+              finalmente el estudiante recibe una triple notificacion por correo
+              electrónico, notificacion web y mobile, tanto en caso que sea
+              aprobada o rechazada.
+            </p>
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -129,14 +184,15 @@ export default function InstructivosPage() {
         onChange={handleChange('panel4')}
       >
         <AccordionSummary aria-controls='panel4d-content' id='panel4d-header'>
-          <Typography>Collapsible Group Item #4</Typography>
+          <Typography>Perfil</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
+            Sección donde puede visualizar los datos de su perfil, nombre,
+            apellido, documento, correo electrónico, fecha de nacimiento,
+            teléfono y domicilio. Tambien puede agregar una foto de perfil. Los
+            únicos datos que está permitido modificar son la foto, domicilio y
+            el teléfono.
           </Typography>
         </AccordionDetails>
       </Accordion>
