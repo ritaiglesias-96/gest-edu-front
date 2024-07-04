d8673f90727f1573ea42f02f71a3835404d6099d
Maria Rita Iglesias <rita.iglesias.adrover@gmail.com>
2024-07-04 17:45:14 -0300

    Arreglo instructivos

--- app/estudiante/instructivos/page.tsx
+++ app/estudiante/instructivos/page.tsx
@@ -2,10 +2,5 @@
 import InstructivosPage from '@/components/Instructivos/instructivo';
 
 export default function InstructivosEstudiante() {
-  return (
-    <div className='flex h-screen flex-col items-center justify-center'>
-      <h1>Instructivos</h1>
-      <InstructivosPage />
-    </div>
-  );
+  return <InstructivosPage />;
 }
